1444686f9fb428adcece4ebbdd080b74d6a3cd53
Hasan <hasanamir8901@gmail.com>
2026-09-11 06:57:26 +0500

    Add: sum-of-all-subset-xor-totals - submission-0

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int dfs(int i, int total, vector<int>& nums){
+        if(i == nums.size()) return total;
+        return dfs(i+1, total^nums[i], nums) + dfs(i+1, total, nums);
+    }
+    int subsetXORSum(vector<int>& nums) {
+        return dfs(0,0, nums);
+    }
+};
